fe8cb383496b51c1e3c763c14b922d54ab905cb0
swhitewat <swhitewat@arizona.edu>
2026-04-20 13:02:04 -0700

    basic code for ingest.py file

---
+++ ingest.py
@@ -0,0 +1,19 @@
+import os
+import fitz  # pymupdf
+import chromadb
+from chromadb.utils import embedding_functions
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# extract text from PDF
+
+
+# heuristic chunking 
+
+
+# load
+
+
+
+# main 
